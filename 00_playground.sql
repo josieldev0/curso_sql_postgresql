739f0c9d3f24c42c0eef4537f6591b6ea5a09355
josieldev0 <josiel.lira01@gmail.com>
2026-01-28 13:43:06 -0300

    Create playground
    Estudos práticos de SQL com PostgreSQL, desenvolvidos a partir de um curso completo, organizados por tópicos e exercícios numerados, abordando desde os fundamentos até conceitos intermediários de banco de dados.

---
+++ 00_playground.sql
@@ -0,0 +1,166 @@
+/* =====================================================
+   Arquivo: 00_playground.sql
+   Autor: Josiel de Lira Barros
+   Descrição:Estudos práticos de SQL com PostgreSQL, desenvolvidos a partir de um curso completo,
+   organizados por tópicos e exercícios numerados, abordando desde os fundamentos até conceitos intermediários de banco de dados.
+===================================================== */
+
+-- =====================================================
+-- CREATE TABLE	
+-- =====================================================
+create table cliente (
+	idcliente integer not null, -- inteiro
+	nome varchar(50) not null, -- até 50 caracteres
+	cpf char(11), -- obrigatoriamente 11 caracteres
+	rg varchar(15),
+	data_nascimento date,
+	genero char(1),
+	profissao varchar(30),
+	nacionalidade varchar(30),
+	logradouro varchar(30),
+	numero varchar(10),
+	complemento varchar(30),
+	bairro varchar(30),
+	municipio varchar(30),
+	uf varchar(30),
+	observacoes text,
+
+	-- primary key
+	constraint pk_cln_idcliente primary key (idcliente)
+	 
+);
+
+-- =====================================================
+-- INSERT INTO / VALUES
+-- =====================================================	
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (1,'Manoel', '88828383821','32323','2001-01-30','M','Estudante', 'Brasileira', 'Rua Joaquim Nabuco','23','Casa','Cidade Nova','Porto Uniao','SC');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (2,'Geraldo', '12343299929','56565','1987-01-04','M','Engenheiro', 'Brasileira', 'Rua das Limas','200','Ap','Centro','Poro Uniao','SC');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (3,'Carlos','87732323227','55463','1967-10-01','M','Pedreiro','Brasileira','Rua das Laranjeiras','300','Apart.','Cto.','Canoinhas','SC');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (4,'Adriana','12321222122','98777','1989-09-10','F','Jornalista','Brasileira','Rua das Limas','240','Casa','São Pedro','General Carneiro','PR');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (5,'Amanda','99982838828','28382','1991-03-04','F','Jorn.','Italiana','Av. Central','100',null, 'São Pedro','General Carneiro','PR');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (6, 'Ângelo', '99982828181','12323','2000-01-01','M','Professor','Brasileiro','Av. Beira Mar','300',null, 'Ctr.','São Paulo','SP');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (7,'Anderson',null,null,null,'M','Prof.','Italiano', 'Av. Brasil','100','Apartamento','Santa Rosa','Rio de Janeiro','SP');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (8,'Camila','9998282828',null,'2001-10-10','F','Professora','Norte-Americana','Rua Central','4333',null,'Centro','Porto Alegre','RS');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (9,'Cristiano',null,null,null,'M','Estudante','Alemã','Rua do Centro','877','Casa','Centro','Porto Alegre','RS');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (10, 'Fabricio','8828282828','32323',null,'M','Estudante','Brasileiro',null,null,null,null, 'PU','SC');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (11,'Fernanda',null,null,null,'F',null,'Brasileira',null,null,null,null,'Porto União','SC');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (12,'Gilmar','88881818181','888','2000-02-10','M','Estud.',null,'Rua das Laranjeiras','200',null,'C. Nova','Canoinhas','SC');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (13,'Diego','1010191919','111939',null,'M','Professor','Alemão','Rua Central','455','Casa','Cidade N.','São Paulo','SP');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (14,'Jeferson',null,null,'1983-07-01','M',null,'Brasileiro',null,null, null,null,'União da Vitória','PR');
+
+insert into cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
+values (15,'Jessica',null,null,null,'F','Estudante', null, null, null, null, null, 'União da Vitória','PR');
+
+-- =====================================================
+-- SELECT
+-- =====================================================
+select * from cliente;
+
+select nome, data_nascimento from cliente;
+
+select nome, data_nascimento as "Data de nascimento" from cliente;
+
+select cpf, rg from cliente;
+
+select 'CPF: ' || cpf || 'RG: ' || rg as "CPF e RG" from cliente;
+
+select * from cliente limit 3;
+
+select nome, data_nascimento from cliente where data_nascimento > '2000-01-01';
+
+select nome from cliente where nome like 'C%';
+
+select nome from cliente where nome like '%c%';
+
+select nome, data_nascimento from cliente where data_nascimento between '1990-01-01' and '1998-01-01';
+
+select nome, rg from cliente where rg is null;
+
+select nome from cliente order by nome asc;
+
+select nome from cliente order by nome desc;
+
+-- =====================================================
+-- EXERCÍCIOS SELECT
+-- =====================================================
+-- 1.	O nome, o gênero e a profissão de todos os clientes, ordenado pelo nome em ordem decrescente
+select nome, genero, profissao from cliente order by nome desc;
+
+-- 2.	Os clientes que tenham a letra “R” no nome
+select * from cliente where nome like '%a%';
+
+-- 3.	Os clientes que o nome inicia com a letra “C”
+select * from cliente where nome like 'C%';
+
+-- 4.	Os clientes que o nome termina com a letra “A”
+select * from cliente where nome like '%a';
+
+-- 5.	Os clientes que moram no bairro “Centro”
+select * from cliente where bairro = 'Centro' or bairro = 'Cto.' or bairro = 'Ctr.';
+
+-- 6.	Os clientes que moram em complementos que iniciam com a letra “A”
+select * from cliente where complemento like 'A%';
+
+-- 7.	Somente os clientes do sexo feminino
+select * from cliente where genero like 'F';
+
+-- 8.	Os clientes que não informaram o CPF
+select * from cliente where cpf is null;
+
+-- 9.	O nome e a profissão dos clientes, ordenado em ordem crescente pelo nome da profissão
+select nome, profissao from cliente order by profissao asc;
+
+-- 10.	Os clientes de nacionalidade “Brasileira”
+select * from cliente where nacionalidade like 'Brasil%';
+
+-- 11.	Os clientes que informaram o número da residência
+select * from cliente where numero is not null;
+
+-- 12.	Os clientes que moram em Santa Catarina
+select * from cliente where uf like 'SC';
+
+-- 13.	Os clientes que nasceram entre 01/01/2000 e 01/01/2002
+select * from cliente where data_nascimento between '2000-01-01' and '2002-01-01';
+
+-- 14.	O nome do cliente e o logradouro, número, complemento, bairro, município e UF concatenado de todos os clientes
+select 'Nome: ' || nome || ' - ' || 'Logradouro: ' || logradouro || ' - ' || 'Número: ' || numero || ' - ' || 'Complemento: ' || complemento || ' - ' || 'Bairro: ' || bairro || ' - ' || 'Município: ' || municipio || ' - ' || 'UF: ' || uf from cliente;
+
+-- =====================================================
+-- UPDATE E DELETE
+-- =====================================================
+select * from cliente;
+
+update cliente set nome = 'Teste' where idcliente = 1;
+
+update cliente set nome = 'Adriano', genero = 'M', numero = '241' where idcliente = 4;
+
+insert into cliente (idcliente, nome) values(16, 'João');
+
+delete from cliente where idcliente = 16;
