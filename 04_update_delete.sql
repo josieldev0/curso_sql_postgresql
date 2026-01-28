af8b291e56d11b44e709b5d0d854b5bd99a686bb
josieldev0 <josiel.lira01@gmail.com>
2026-01-28 13:47:48 -0300

    Create 04_update_delete

---
+++ 04_update_delete.sql
@@ -0,0 +1,12 @@
+-- =====================================================
+-- UPDATE E DELETE
+-- =====================================================
+select * from cliente;
+
+update cliente set nome = 'Teste' where idcliente = 1;
+
+update cliente set nome = 'Adriano', genero = 'M', numero = '241' where idcliente = 4;
+
+insert into cliente (idcliente, nome) values(16, 'João');
+
+delete from cliente where idcliente = 16;
