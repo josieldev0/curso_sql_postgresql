26db8ddf186498574206acaad51437bf724717de
josieldev0 <josiel.lira01@gmail.com>
2026-02-02 19:11:57 -0300

    Create 05_alter_table

---
+++ 05_alter_table.sql
@@ -0,0 +1,57 @@
+-- =====================================================
+-- ALTER TABLE
+-- =====================================================
+alter table cliente rename column profissao to idprofissao;
+alter table cliente alter column idprofissao type integer;
+alter table cliente drop idprofissao;
+alter table cliente add idprofissao integer;
+alter table cliente add constraint fk_cln_idprofissao foreign key (idprofissao) references profissao (idprofissao);
+
+update cliente set idprofissao = 1 where idcliente in (1, 9, 10, 12, 15, 17);
+update cliente set idprofissao = 2 where idcliente = 2;
+update cliente set idprofissao = 3 where idcliente = 3;
+update cliente set idprofissao = 4 where idcliente in (4, 5);
+update cliente set idprofissao = 5 where idcliente in (6, 7, 8, 13);
+
+insert into profissao (idprofissao, nome) values (10, 'Teste');
+delete from profissao where idprofissao = 10;
+
+alter table cliente drop nacionalidade;
+alter table cliente add idnacionalidade integer;
+alter table cliente add constraint fk_cln_idnacionalida foreign key (idnacionalidade) references nacionalidade (idnacionalidade);
+
+update cliente set idnacionalidade = 1 where idcliente in (1, 2, 3, 4, 6, 10, 11, 14);
+update cliente set idnacionalidade = 2 where idcliente in (5, 7);
+update cliente set idnacionalidade = 3 where idcliente = 8;
+update cliente set idnacionalidade = 4 where idcliente in (9, 13);
+
+alter table cliente drop complemento;
+alter table cliente add idcomplemento integer;
+alter table cliente add constraint fk_cln_idcomplemento foreign key (idcomplemento) references complemento (idcomplemento);
+
+update cliente set idcomplemento = 1 where idcliente in (1, 4, 9, 13);
+update cliente set idcomplemento = 2 where idcliente in (2, 3, 7);
+
+alter table cliente drop bairro;
+alter table cliente add idbairro integer;
+alter table cliente add constraint fk_cln_idbairro foreign key (idbairro) references bairro (idbairro);
+
+update cliente set idbairro = 1 where idcliente in (1, 12, 13);
+update cliente set idbairro = 2 where idcliente in (2, 3, 6, 8, 9);
+update cliente set idbairro = 3 where idcliente in (4, 5);
+update cliente set idbairro = 4 where idcliente = 7;
+
+alter table cliente drop municipio;
+alter table cliente drop uf;
+alter table cliente add idmunicipio integer;
+alter table cliente add constraint fk_cliente_idmunicipio foreign key (idmunicipio) references municipio (idmunicipio);
+
+update cliente set idmunicipio = 1 where idcliente in (1, 2, 10, 11);
+update cliente set idmunicipio = 2 where idcliente in (3, 12);
+update cliente set idmunicipio = 3 where idcliente = 4;
+update cliente set idmunicipio = 4 where idcliente = 5;
+update cliente set idmunicipio = 5 where idcliente in (6, 13);
+update cliente set idmunicipio = 6 where idcliente = 7;
+update cliente set idmunicipio = 7 where idcliente = 8;
+update cliente set idmunicipio = 8 where idcliente = 9;
+update cliente set idmunicipio = 9 where idcliente in (14, 15);
