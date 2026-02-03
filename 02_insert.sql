59c215f59d8bb4fdfb64da203dcddd560a14f03a
josieldev0 <josiel.lira01@gmail.com>
2026-02-02 21:51:27 -0300

    Update insert

--- 02_insert.sql
+++ 02_insert.sql
@@ -84,3 +84,49 @@ insert into municipio (idmunicipio, nome, iduf) values (6, 'Rio de Janeiro', 6);
 insert into municipio (idmunicipio, nome, iduf) values (7, 'Uberlândia', 4);
 insert into municipio (idmunicipio, nome, iduf) values (8, 'Porto Alegre', 5);
 insert into municipio (idmunicipio, nome, iduf) values (9, 'União da Vitória', 2);
+
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+values (1, '2008-04-01', 1300, 1, 1, 1);
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+values (2, '2008-04-01', 500, 1, 1, 1);
+insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
+values (3, '2008-04-02', 300, 11, 2, 5);
+Insert into pedido (idpedido, data_pedido, valor, idcliente,idtransportadora, idvendedor) values (4, '2008-04-05', 1000, 8, 1, 7);
+Insert into pedido (idpedido, data_pedido, valor, idcliente,idtransportadora, idvendedor) values (5, '2008-04-06', 200, 9, 2, 6);
+Insert into pedido (idpedido, data_pedido, valor, idcliente,idtransportadora, idvendedor) values (6, '2008-04-06', 1985, 10, 1, 6);
+Insert into pedido (idpedido, data_pedido, valor, idcliente,idtransportadora, idvendedor) values (7, '2008-04-06', 800, 3, 1, 7);
+Insert into pedido (idpedido, data_pedido, valor, idcliente,idtransportadora, idvendedor) values (8, '2008-04-06', 175, 3, null, 7);
+Insert into pedido (idpedido, data_pedido, valor, idcliente,idtransportadora, idvendedor) values (9, '2008-04-07', 1300, 12, null, 8);
+Insert into pedido (idpedido, data_pedido, valor, idcliente,idtransportadora, idvendedor) values (10, '2008-04-10', 200, 6, 1, 8);
+Insert into pedido (idpedido, data_pedido, valor, idcliente,idtransportadora, idvendedor) values (11, '2008-04-15', 300, 15, 2, 1);
+Insert into pedido (idpedido, data_pedido, valor, idcliente,idtransportadora, idvendedor) values (12, '2008-04-20', 300, 15, 2, 5);
+Insert into pedido (idpedido, data_pedido, valor, idcliente,idtransportadora, idvendedor) values (13, '2008-04-20', 350, 9, 1, 7);
+Insert into pedido (idpedido, data_pedido, valor, idcliente,idtransportadora, idvendedor) values (14, '2008-04-23', 300, 2, 1, 5);
+Insert into pedido (idpedido, data_pedido, valor, idcliente,idtransportadora, idvendedor) values (15, '2008-04-25', 200, 11, null, 5);
+
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario) values
+(1, 1, 1, 800);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario) values
+(1, 2, 1, 500);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario) values
+(2, 2, 1, 500);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (3, 4, 2, 150);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (4, 1, 1, 800);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (4, 3, 1, 200);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (5, 3, 1, 200);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (6, 1, 2, 800);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (6, 7, 1, 35);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (6, 5, 1, 200);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (6, 4, 1, 150);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (7, 1, 1, 800);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (8, 7, 5, 35);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (9, 1, 1, 800);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (9, 2, 1, 500);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (10, 5, 1, 200);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (11, 5, 1, 200);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (11, 6, 1, 100);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (12, 2, 1, 500);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (13, 3, 1, 200);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (13, 4, 1, 150);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (14, 6, 3, 100);
+Insert into pedido_produto (idpedido,idproduto,quantidade,valor_unitario) values (15, 3, 1, 200);
