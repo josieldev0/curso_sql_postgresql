84d0eddc8bcdd448a1d8e775e8b205eea6dda06a
josieldev0 <josiel.lira01@gmail.com>
2026-03-15 01:31:01 -0300

    Update exercicios

--- exercicios.sql
+++ exercicios.sql
@@ -610,3 +610,71 @@ left outer join
 	 pedido on pedido_produto.idpedido = pedido.idpedido
 group by 
 	pedido.data_pedido
+
+-- =====================================================
+-- EXERCÍCIOS COMANDOS ADICIONAIS
+-- =====================================================
+-- 1. O nome do cliente e somente o mês de nascimento. Caso a data de nascimento não esteja preenchida mostrar a mensagem “Não informado”.
+select
+	nome,
+	coalesce(extract(month from data_nascimento), 0) as mes_nascimento
+from 
+	cliente
+
+-- 2. O nome do cliente e somente o nome do mês de nascimento (Janeiro, Fevereiro etc). Caso a data de nascimento não esteja preenchida mostrar a mensagem “Não informado”.
+select 
+	nome,
+	case extract(month from data_nascimento)
+		when 1 then 'Janeiro'
+		when 2 then 'Fevereiro'
+		when 3 then 'Março'
+		when 4 then 'Abril'
+		when 5 then 'Maio'
+		when 6 then 'Junho'
+		when 7 then 'Julho'
+		when 8 then 'Agosto'
+		when 9 then 'Setembro'
+		when 10 then 'Outubro'
+		when 11 then 'Novembro'
+		when 12 then 'Dezembro'
+	else
+		'Não informado'
+	end as mes_nascimento
+from 
+	cliente
+
+-- 3. O nome do cliente e somente o ano de nascimento. Caso a data de nascimento não esteja preenchida mostrar a mensagem “Não informado”.
+select
+	nome,
+	coalesce(extract(year from data_nascimento), 0) as ano_nascimento
+from
+	cliente
+
+-- 4. O caractere 5 até o caractere 10 de todos os municípios.
+select substring(nome from 5 for 10) as municipio_cortado from municipio
+
+-- 5. O nome de todos os municípios em letras maiúsculas.
+select upper(nome) as municipio_maiusculo from municipio
+
+-- 6. O nome do cliente e o gênero. Caso seja M mostrar “Masculino”, senão mostrar “Feminino”.
+select
+	nome,
+	case genero
+		when 'M' then 'Masculino'
+		when 'F' then 'Feminino'
+	else 'Não informado'
+	end as genero
+from 
+	cliente
+
+-- 7. O nome do produto e o valor. Caso o valor seja maior do que R$ 500,00 mostrar a mensagem “Acima de 500”, caso contrário, mostrar a mensagem “Abaixo de 500”.
+select 
+	nome,
+	valor,
+	case 
+		when valor >= 500 then 'Acima ou igual a 500'
+	else 
+		'Abaixo de 500'
+	end as faixa
+from 
+	produto
